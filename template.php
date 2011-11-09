6019fd3075c1310cd6eaa0a379dfc2991d20e7ab
Reed La Botz <reedlabotz@gmail.com>
2011-11-09 12:54:12 -0600

    Add color scheme

---
+++ template.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+   <title>Better Campus Map</title>
+	<link rel="stylesheet" href="styles/ui.css" type="text/css" media="screen" title="no title" charset="utf-8">
+	<link rel="stylesheet" href="styles/master.css" type="text/css" media="screen" title="no title" charset="utf-8">
+</head>
+<body>
+   <div id="holder">
+      <div id="header">
+         <div id="logo" style="background:#cccccc;width:200px;height:75px;">Better Campus Map</div>
+         <div id="search">
+            <input type="search"><input type="submit" value="Search" id="submit_search">
+         </div>
+         <div class="clear"></div>
+      </div>
+      <div id="main">
+         Main content
+      </div>
+      <div id="footer">
+         Footer
+      </div>
+   </div>
+</body>
+</html>
